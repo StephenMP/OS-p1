3b830d83ec909f0c47389fb93448213a1db66ef4
root <root@localhost.localdomain>
2014-09-24 14:28:25 -0600

    Got the parser done and it works as expected. I am not able to execute jobs. Also took care of the cd and exit commands as well as provide skeletal code for the jobs command

--- mydash.c
+++ mydash.c
@@ -27,7 +27,30 @@ int main(int argc, char *args[])
 
 void execute(char **argv, char *command, Boolean isBackground)
 {
+	/* Fork our child */
+	pid_t pid = fork();
 
+	/* Hook to track status */
+	int status = 0;
+
+	/* Attempt to fork a child proc */
+	if (pid < 0) {
+		err_sys("Error forking");
+	}
+
+	/* We are the child */
+	if (pid == 0) {
+		/* Have the child execute the command */
+		int result = execvp(*argv, argv);
+
+		/* Handle any forking errors */
+		if (result < 0)
+			err_sys("Exec failed");
+	}
+	/* We are the parent */
+	else{
+		while (wait(&status) != pid) ;
+	}
 }
 
 void changeDirectory(char* path)
@@ -51,6 +74,38 @@ int initShell()
 		command = (char*)malloc(sizeof(char) * (strlen(line) + 1));
 		strcpy(command, line);
 
+		/* Handle if user just pressed enter or input just whitespace */
+		if (parseEmptyLine(line) == TRUE) {
+			free(line);
+			free(command);
+			continue;
+		}
+
+		/* Insert line into the history for recall */
+		add_history(command);
+
+		/* Parse the user input */
+		parseCommand(line, argv);
+
+		/* Handle exit */
+		if (strcmp(argv[0], "exit") == EQUAL) break;
+
+		/* Handle cd */
+		if (strcmp(argv[0], "cd") == EQUAL) {
+			changeDirectory(argv[1]);
+			free(command);
+			free(line);
+			continue;
+		}
+
+		/* Handle jobs command */
+		if (strcmp(argv[0], "jobs") == EQUAL) {
+			printf("Not yet supported");
+			continue;
+		}
+
+		/* Execute a job */
+		execute(argv, command, backgroundJob);
 
 		/* Keep it clean */
 		free(command);
